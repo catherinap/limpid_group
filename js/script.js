8611f33844acb056a0b06f0c85b367aa905fcb4c
Kateryna Panchenko <buddhagolden@gmail.com>
2019-11-20 15:35:14 +0200

    Fix: fixed header height

--- js/script.js
+++ js/script.js
@@ -1,29 +1,34 @@
+window.addEventListener('DOMContentLoaded', function () {
+
 //Slider
-function showSlides() {
-    let slideIndex = 1,
-        slides = document.querySelectorAll('#gallery .photos img'),
-        dots = document.querySelectorAll('.slider_btn');
+    function showSlides() {
+        let slideIndex = 1,
+            slides = document.querySelectorAll('#gallery .photos img'),
+            dots = document.querySelectorAll('.slider_btn');
 
-    for (let i = 1; i < dots.length; i++) {
+        for (let i = 1; i < dots.length; i++) {
 
-        dots[i].addEventListener('click', function () {
-            for (let i = 0; i < slides.length; i++) {
-                slides[i].classList.remove('showed');
-            }
-            slideIndex++;
+            dots[i].addEventListener('click', function () {
+                for (let i = 0; i < slides.length; i++) {
+                    slides[i].classList.remove('showed');
+                }
+                slideIndex++;
 
-            if (slideIndex > slides.length) {
-                slideIndex = 1;
-            }
+                if (slideIndex > slides.length) {
+                    slideIndex = 1;
+                }
 
-            for (let i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" red", "");
-            }
-            slides[slideIndex - 1].classList.add('showed');
-            dots[slideIndex - 1].className += " red";
-        });
+                for (let i = 0; i < dots.length; i++) {
+                    dots[i].className = dots[i].className.replace(" red", "");
+                }
+                slides[slideIndex - 1].classList.add('showed');
+                dots[slideIndex - 1].className += " red";
+            });
+        }
+        setTimeout(showSlides, 6000);
     }
-    setTimeout(showSlides, 6000);
-}
 
-showSlides();
+    showSlides();
+});
+
+
