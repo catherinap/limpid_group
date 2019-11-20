c6dbdfffd077e80b4efcf7f7e770ba43defab55c
Kateryna Panchenko <buddhagolden@gmail.com>
2019-11-20 13:31:00 +0200

    Basic layout finished

--- js/script.js
+++ js/script.js
@@ -1,28 +1,29 @@
 //Slider
 function showSlides() {
     let slideIndex = 1,
-    slides = document.querySelectorAll('#gallery .photos img'),
-    dots = document.querySelectorAll('.slider_btn');
+        slides = document.querySelectorAll('#gallery .photos img'),
+        dots = document.querySelectorAll('.slider_btn');
 
-   for(let i = 1; i < dots.length; i++) {
+    for (let i = 1; i < dots.length; i++) {
 
-       dots[i].addEventListener('click', function () {
-           for (let i = 0; i < slides.length; i++) {
-               slides[i].classList.remove('showed');
-           }
-           slideIndex++;
+        dots[i].addEventListener('click', function () {
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].classList.remove('showed');
+            }
+            slideIndex++;
 
-           if (slideIndex > slides.length) {
-               slideIndex = 1;
-           }
+            if (slideIndex > slides.length) {
+                slideIndex = 1;
+            }
 
-           for (let i = 0; i < dots.length; i++) {
-               dots[i].className = dots[i].className.replace(" red", "");
-           }
-           slides[slideIndex - 1].classList.add('showed');
-           dots[slideIndex - 1].className += " red";
-       });
-   }
-   setTimeout(showSlides, 6000);
+            for (let i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" red", "");
+            }
+            slides[slideIndex - 1].classList.add('showed');
+            dots[slideIndex - 1].className += " red";
+        });
+    }
+    setTimeout(showSlides, 6000);
 }
+
 showSlides();
